2e1594c6880824c33915543a36ee2c125e1a2ba2
KamielFlamiel <justinmeijer2007@outlook.com>
2025-05-21 09:02:03 +0200

    header

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+        <div class="header-container">
+            <div class="header-logo">
+                <img class="logo-image" src="img/Teto_logo" alt="">
+            </div>
+        </div>
+    </header>
