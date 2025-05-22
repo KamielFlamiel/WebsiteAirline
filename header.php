014b42dace148f8974c4495336fc8a637a6ec829
KamielFlamiel <justinmeijer2007@outlook.com>
2025-05-22 11:53:37 +0200

    nav bar toegevoegd

--- header.php
+++ header.php
@@ -1,7 +1,16 @@
 <header>
-        <div class="header-container">
-            <div class="header-logo">
-                <img class="logo-image" src="img/Teto_logo.png" alt="">
-            </div>
-        </div>
+    <div class="header-container">
+        <nav class="nav-bar">
+            <ul>
+                <li><a href="index.php">Flights</a></li>
+                <li><a href="#locations">Locations</a></li>
+                <li><a href="#about">About us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div class="header-logo">
+        <img src="img/Teto_logo.png" alt="">
+    </div>
 </header>
