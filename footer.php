c5268d6412fdea10fd0e0ba21badc255bfd84727
LoraineMP3 <soxngd2@gmail.com>
2025-06-05 11:08:59 +0200

    Progress footer

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
 <footer>
-        <div class="footer-container"></div>
+        <div class="footer-container">
+            <div class="contact-payment">
+        <img class="footer-image" src="img/Group-7.png" alt="">
+    </div>
+    <div class="footer-titles">
+        <h1 class="titles">Follow us:</h1>
+        <h1 class="titles">Populair Locations:</h1>
+        <h1 class="titles">Pay safetly with:</h1>
+    </div>
+    <div class="popular-locations">
+        <h2 class="locations">Tetoland</h2>
+        <h2 class="locations">Teto Island</h2>
+        <h2 class="locations">Little Saint Miku</h2>
+        <h2 class="locations">Tetograd</h2>
+    </div>
+        </div>
     </footer>
